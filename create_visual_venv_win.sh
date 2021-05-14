cdae47eb57340fc2a7f8183d4a31320629c41650
NormalReedus <magnus.borregaard@gmail.com>
2021-05-14 14:08:49 +0200

    scripts work

--- create_visual_venv_win.sh
+++ create_visual_venv_win.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-VENVNAME=testvenv
+VENVNAME=cv101
 
 python -m venv $VENVNAME
 source $VENVNAME/Scripts/activate
 python get-pip.py
-# pip install --upgrade pip
 
-test -f testreq.txt && pip install -r testreq.txt
+test -f requirements.txt && pip install -r requirements.txt
 
 # Makes sure the required directories are present (since git does not clone empty folders)
 mkdir -p data/
 mkdir -p features/
+mkdir -p output/
 
 echo "build $VENVNAME"
--- make_dirs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Makes sure the required directories are present (since git does not clone empty folders)
-mkdir -p data/
-mkdir -p features/
