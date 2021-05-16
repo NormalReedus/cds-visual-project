016d5f8b9e9d0d69b33e90bf1ab0e99ce14ea901
Mikkel Ottosen <mikkel.mokasin@gmail.com>
2021-05-16 15:14:54 +0200

    .

--- src/0_get_data.py
+++ src/0_get_data.py
@@ -8,8 +8,16 @@ def filter_only_images(dir_path, valid_suffix):
     dir_contents = os.listdir(dir_path)
     for content in dir_contents:
         _, extension = os.path.splitext(content)
-        if extension is not valid_suffix:
-            os.remove(os.path.join(dir_path, content))
+        content_path = os.path.join(dir_path, content)
+
+        if extension.lower() != valid_suffix:
+            print(extension)
+            print(valid_suffix)
+            if os.path.isdir(content_path):
+                shutil.rmtree(content_path)
+            else:
+                os.remove(content_path)
+        
 
 #Move all files in a folder to another folder
 def move_files(source_folder_path, target_folder_path):
@@ -17,25 +25,19 @@ def move_files(source_folder_path, target_folder_path):
     for file in file_names:
         shutil.move(os.path.join(source_folder_path, file), target_folder_path)
 
-def run():
-    #Setup auth for Kaggle. Authenticate() needs a valid .kaggle json
-    # api = KaggleApi()
-    # api.authenticate()
-    # api.dataset_download_files('prithvijaunjale/instagram-images-with-captions', path='../data', unzip = True, quiet=False)
+def run(data_dir_path):
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files('prithvijaunjale/instagram-images-with-captions', path='../data', unzip = True, quiet=False)
 
-    yeet = os.walk(os.path.join('..', 'data'))
-    print(yeet.files)
-    
-    #Move around the files
-    # data_dir_path = os.path.join('..', 'data')
-    # data_subfolders = [content.path for content in os.scandir(data_dir_path) if os.path.isdir(content.path)]
-    # for data in data_subfolders:
-    #     dirs = os.scandir(data)
-    #     for dir in dirs:
-    #         print(dir.path)
+    move_files(os.path.join('..', 'data', 'instagram_data', 'img'), os.path.join('..', 'data'))
+    move_files(os.path.join('..', 'data', 'instagram_data2', 'img2'), os.path.join('..', 'data'))
+    print('Moved all files')
 
-    # data_subfolders = [content.path for content in os.scandir(data_dir_path) if content.is_dir()]
-    
+    print('Trying to remove anything not images')
+    filter_only_images(os.path.join('..','data'), '.jpg')
+
+            
 
 if __name__ == '__main__':
-    run()
+    run(os.path.join('..', 'data'))
